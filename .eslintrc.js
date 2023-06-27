93332bebbf7dc304105c73e062e787e0d971de4f
RafaelZamoraBallerTV <rafael@baller.tv>
2023-06-27 14:37:06 -0700

    Adds config file

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: 'airbnb',
+    overrides: [
+        {
+            env: {
+                node: true,
+            },
+            files: [
+                '.eslintrc.{js,cjs}',
+            ],
+            parserOptions: {
+                sourceType: 'script',
+            },
+        },
+    ],
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+    },
+    //  Can override any rules we disagree with here
+    // https://github.com/airbnb/javascript/tree/master/packages/eslint-config-airbnb/rules
+    rules: {
+        // example of overriding a rule
+        // 'no-console': ['error', { allow: ['warn', 'error'] }],
+    },
+};

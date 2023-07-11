b4edc83d91dcf7d2a3a77d43eff38d23a730f091
RafaelZamoraBallerTV <rafael@baller.tv>
2023-07-11 16:20:34 -0700

    Adds prettier configs

---
+++ .prettierrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    trailingComma: 'all',
+    tabWidth: 2,
+    semi: true,
+    singleQuote: true,
+    printWidth: 80,
+    arrowParens: 'avoid',
+    bracketSpacing: true,
+    endOfLine: 'auto',
+    htmlWhitespaceSensitivity: 'css',
+    proseWrap: 'preserve',
+    quoteProps: 'as-needed',
+};
